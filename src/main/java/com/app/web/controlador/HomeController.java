c89b1d126621afd9b86165debb6ef2ade763ed5c
Rodrigit-hub <rodri98alves@gmail.com>
2024-10-21 23:23:07 -0300

    agregando redireccion al index en caso de logueo

--- src/main/java/com/app/web/controlador/HomeController.java
+++ src/main/java/com/app/web/controlador/HomeController.java
@@ -1,4 +1,6 @@
 package com.app.web.controlador;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -8,6 +10,15 @@ public class HomeController {
 
     @GetMapping("/login")
     public String login() {
+    	// Obtener la autenticación actual
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        // Verificar si el usuario ya está autenticado
+        if (authentication != null && authentication.isAuthenticated() && 
+            !authentication.getPrincipal().equals("anonymousUser")) {
+            // Redirigir a la página de inicio si está logueado
+            return "redirect:/index";
+        }
         return "login";
     }
 
